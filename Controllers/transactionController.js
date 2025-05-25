74f9010df86515ed902f393e45951b2736dae56c
Fasogba ayomide Isaac <144702636+Topsyfash@users.noreply.github.com>
2025-05-25 08:42:09 +0100

    Update transactionController.js

--- Controllers/transactionController.js
+++ Controllers/transactionController.js
@@ -23,10 +23,6 @@ const updateWalletBalance = async (req, res) => {
         }
 
         
-         if (amount <= 0) {
-            return res.status(400).json({ message: "Amount Must be Greater Than 0" });
-        }
-        
         const userWallet = await Wallet.findOneAndUpdate(
             { user_id: userId },
             { $inc: { balance: amount } }
